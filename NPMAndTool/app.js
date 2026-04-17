0248d112344afebe1ff27545c91efe593be39b1d
Sajid <sajidahmadgpt@gmail.com>
2026-04-17 08:55:09 +0530

    feat: initialize basic Node.js HTTP server project with start script

---
+++ NPMAndTool/app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const { requestHandler } = require('./handler');
+
+const server = http.createServer((req, res) => {
+    console.log(req);
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
