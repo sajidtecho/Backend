911d36e2a413d76d1c4e5a668b7ffc947b1700c9
Sajid <sajidahmadgpt@gmail.com>
2026-04-17 09:59:35 +0530

    feat: add area calculation practice script with basic conditional logic

---
+++ ErrorAndDebugging/Practice.js
@@ -0,0 +1,21 @@
+function calculateArea(width, height) {
+    return width + height;
+}
+
+let width = 10, height = 5;
+
+let area = calculateArea(width, height);
+
+if (area > 100) {
+    console.log("The area is large.");
+}
+else {
+    console.log("The area is small.");
+}
+
+if (area > 100) {
+    console.log("Area is greater than or equal to 100")
+}
+
+
+module.exports = calculateArea;
