499ba2c055a8ab5601346bb6425a8695e1bff005
Sajid <sajidahmadgpt@gmail.com>
2026-04-24 10:33:52 +0530

    feat: initialize project with Tailwind CSS configuration and base styling structure

---
+++ Practice/airbnbStyling/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./views/**/*.html",
+    "./public/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
